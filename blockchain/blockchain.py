8d20b28201e6e8e103381b95ede59723b3b89155
Ritesh Srivastava <rsrivastava76@hotmail.com>
2018-04-15 23:23:10 +0530

    adding basic building functions for creating block chain execution code

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -18,3 +18,23 @@ class Blockchain:
         self.chain.append(block)
         return block
 
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof = new_proof + 1
+
+        return new_proof
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys= True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    #def isChain_valid(self):
